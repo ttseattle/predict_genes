bf9fd7912f552fe041c5916c26f8e4ec1530125b
TISHA TRONGTHAM <tishat@cs.washington.edu>
2016-11-03 21:35:14 -0700

    almost ready for turnin

--- hw2.py
+++ hw2.py
@@ -1,51 +1,50 @@
-import open_reading_frame 
+import open_reading_frame
 import markov_model
 import matplotlib.pyplot as plt
-import numpy as np
 import pdb
-from numpy import trapz
+import numpy as np
 import time
-start_time = time.time()
 
 MJANNASCHII_FILEPATH = "data/genome.fna"
 ANNOTATIONS_FILEPATH = "data/annotation.gbff"
 SHORT_THRESH = 50
 LONG_THRESH = 1400
-K = 3 
+K = 3
 
-
-
-def main(): 
-    # get data 
-    seq = open_reading_frame.prep_mjannaschii(MJANNASCHII_FILEPATH) 
+def main():
+    # get data
+    seq = open_reading_frame.prep_mjannaschii(MJANNASCHII_FILEPATH)
     gene_ends = open_reading_frame.prep_annotations(ANNOTATIONS_FILEPATH, seq)
 
+    # get ORFs
     orfs = find_orfs(seq)
+
+    # get training data
     short_orfs, long_orfs = find_short_and_long_orfs(orfs, SHORT_THRESH, LONG_THRESH)
     get_num_cdss(gene_ends)
+    trusted_non_genes = remove_stop_codons(short_orfs)
+    trusted_genes = remove_stop_codons(long_orfs)
 
-    trusted_non_genes = prep_training_set(short_orfs)
-    trusted_genes = prep_training_set(long_orfs)
+    # get probabilities for calculating Markov Model scores
     trusted_genes_probs = markov_model.calc_probs(K, trusted_genes)
     trusted_non_genes_probs = markov_model.calc_probs(K, trusted_non_genes)
 
-    # homework problems 
-    
-
-    # scores = orf_mm_scores(orfs, gene_ends, 30, K, trusted_genes, trusted_non_genes)
-    # print mm_thresh_2(orfs, gene_ends, 30, K, trusted_genes, trusted_non_genes, scores)
-    # print mm_thresh_2(orfs, gene_ends, 40, K, trusted_genes, trusted_non_genes, scores)
-    # print mm_thresh_2(orfs, gene_ends, 50, K, trusted_genes, trusted_non_genes, scores)
-    
-
-    # open_reading_frame.len_thresh(orfs, gene_ends, 348)
-    # 
+    # get information about 5 short and 5 long ORFs
     # count_xy(K, trusted_genes, trusted_non_genes)
-    print_summary(short_orfs, long_orfs, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs, gene_ends)
+    # print_summary(short_orfs, long_orfs, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs, gene_ends)
+
+    # # find thresholds to reach 80% true positive rate
+    # reach_len_thresh(orfs, gene_ends, 434) # 435 --> 79.72665148063781%
+    # reach_mm_thresh(orfs, gene_ends, 6.774, K, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs) # 6.775 --> 79.95444191343963%
+
+    # # plot data
+    plot_roc(orfs, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs, gene_ends)
+
+    # plot_scatter(K, orfs, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs, gene_ends, 0.20)
 
 # 1A #####################################################################
 
-def find_orfs(seq): 
+def find_orfs(seq):
     print "NUMBER OF ORFS FOUND"
     orfs = []
 
@@ -64,7 +63,7 @@ def find_short_and_long_orfs(orfs, short_threshold, long_threshold):
     print "NUMBER OF ORFS OF LENGTH < 50 AND > 1400"
     trusted_non_genes = []
     trusted_genes = []
-    for orf in orfs: 
+    for orf in orfs:
         seq, start, end = orf
         if len(seq) < short_threshold:
             trusted_non_genes.append(orf)
@@ -87,26 +86,14 @@ def get_num_cdss(cdss):
 
 def remove_stop_codons(training):
     new_training = []
-    for orf in training: 
-        seq = orf[0]
-        new_seq = seq[:-3]
-        new_training.append((new_seq, orf[1], orf[2]))
-    return new_training
-
-def remove_stop_codon(orf):
-    seq = orf[0]
-    return seq[:-3]
-
-def prep_training_set(training): 
-    new_training = []
-    for orf in training: 
+    for orf in training:
         seq = orf[0]
         new_seq = seq[:-3]
         if len(new_seq) != 0:
             new_training.append(new_seq)
     return new_training
 
-def count_xy(k, trusted_genes, trusted_non_genes): 
+def count_xy(k, trusted_genes, trusted_non_genes):
     print "COUNTS FOR CALCULATING P(A|Axy) -- COUNT(AxyA), COUNT(Axy)"
     count_xy_helper(k, trusted_genes)
 
@@ -118,7 +105,7 @@ def count_xy_helper(k, training):
     for nuc1 in ["A", "C", "G", "T"]:
         counts = []
         for nuc2 in ["A", "C", "G", "T"]:
-            axy = 'A' + nuc1 + nuc2 
+            axy = 'A' + nuc1 + nuc2
             y_seqs = markov_model.find_y(axy, k, training)
             num_y = len(y_seqs)
             num_x = markov_model.count_x('A', k, y_seqs)
@@ -148,155 +135,289 @@ def print_summary_helper(orfs, trusted_genes, trusted_non_genes, trusted_genes_p
         mms.append(mm)
         count += 1
 
-def print_info(count, orf, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs, gene_ends): 
-    seq, start, end = orf 
+def print_info(count, orf, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs, gene_ends):
+    seq, start, end = orf
     mm_score = markov_model.calc_mm_score(seq, K, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs)
-    cds = 'True' if end in gene_ends else 'False' 
+    cds = 'True' if end in gene_ends else 'False'
     print "{:<5} {:<10} {:<10} {:<18} {:<10}".format(count, start, len(seq), mm_score, cds)
     return (len(seq), mm_score)
 
 # 2 ######################################################################
 
-def plot_roc():
-    len_x, len_y = plot_len_roc(1400, orfs, gene_ends)
-    plt.plot(len_x, len_y, 'ro')
-    
-    mm_x, mm_y = plot_mm_roc(100, K, orfs, gene_ends, trusted_genes, trusted_non_genes)
-    plt.plot(mm_x, mm_y, 'ro')
-    
+def plot_roc(orfs, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs, gene_ends):
+    plt.suptitle('ROC Curves')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    axes = plt.gca()
+    axes.set_xlim([0, 1])
+    axes.set_ylim([0, 1])
+
+    len_x, len_y = plot_len_roc(orfs, gene_ends)
+    plt.plot(len_x, len_y, color='red')
+
+    mm_x, mm_y = plot_mm_roc(K, orfs, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs, gene_ends)
+    plt.plot(mm_x, mm_y, color='green')
+
+    combo_x, combo_y = plot_combo_roc(K, orfs, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs, gene_ends)
+    plt.plot(combo_x, combo_y, color='magenta')
+
     # Compute the area using the composite trapezoidal rule.
-    len_area = trapz(len_y, dx=5)
-    print("len ROC area under curve =", len_area)
-    mm_area = trapz(mm_y, dx=5)
-    print("mm ROC area under curve =", mm_area)
-    
+    len_area = np.trapz(len_y, sorted(len_x))
+    print "len ROC area under curve =", len_area
+    mm_area = np.trapz(sorted(mm_y), sorted(mm_x))
+    print "mm ROC area under curve =", mm_area
+    combo_area = np.trapz(sorted(combo_y), sorted(combo_x))
+    print "combo ROC area under curve =", combo_area
+
+    plt.legend(['length', 'markov model', 'combined length & markov model'], loc='lower right')
     plt.show()
 
-def orf_mm_scores(orfs, gene_ends, threshold, k, trusted_genes, trusted_non_genes):
-    mm_scores = []
-    for orf in orfs: 
-        seq, start, end = orf 
-        mm_score = markov_model.calc_mm_score(seq, k, trusted_genes, trusted_non_genes)
-        mm_scores.append((orf, mm_score))
-    return mm_scores 
-
-def plot_len_roc(threshold, orfs, gene_ends):
+def plot_len_roc(orfs, gene_ends):
     x = []
     y = []
-    thresh = 0 
-    while thresh < threshold:
-        thresh += 50
-        x_pt, y_pt = len_thresh(orfs, gene_ends, thresh)
+    thresh = 0
+    while thresh < LONG_THRESH:
+        thresh += 10
+        data = len_thresh(orfs, gene_ends, thresh)
+        x_pt = data[2]
+        y_pt = data[3]
         x.append(x_pt)
         y.append(y_pt)
-    return x, y 
+    return x, y
 
-def plot_mm_roc(threshold, k, orfs, gene_ends, trusted_genes, trusted_non_genes): 
+def plot_mm_roc(k, orfs, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs, gene_ends):
     x = []
     y = []
-    thresh = 0 
-    while thresh < threshold: 
-        thresh += 50
-        x_pt, y_pt = mm_thresh(orfs, gene_ends, threshold, k, trusted_genes, trusted_non_genes)
+    thresh = 0
+    while thresh < 20:
+        thresh += 0.2
+        data = mm_thresh(orfs, gene_ends, thresh, k, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs)
+        x_pt = data[2]
+        y_pt = data[3]
         x.append(x_pt)
         y.append(y_pt)
-    return x, y 
+    return x, y
 
-def get_rates(num_true_positives, num_false_positives, orfs, genes, gene_ends): 
-    # TPR = TP/TP+FN 
-    # TP + FN = CDS 
+def plot_combo_roc(k, orfs, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs, gene_ends): 
+    trusted_gene_x, trusted_gene_y = plot_scatter_helper(k, trusted_genes, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs)
+
+    trusted_non_gene_x, trusted_non_gene_y = plot_scatter_helper(k, trusted_non_genes,trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs)
+
+    a_x, a_y, b_x, b_y, m, b = calc_median(trusted_gene_x, trusted_gene_y, trusted_non_gene_x, trusted_non_gene_y)
+    
+    x = []
+    y = []
+    thresh = 0
+    while thresh < 1: 
+        thresh += 0.05 
+        p1_x, p1_y, p2_x, p2_y, p_m = calc_perpendicular(a_x, b_x, m, b, thresh)
+        data = get_positives_with_equation(k, orfs, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs, gene_ends, p1_x, p1_y, p_m)
+        x_pt = data[2]
+        y_pt = data[3]
+        x.append(x_pt)
+        y.append(y_pt)
+    return x, y
+
+def get_rates(orfs, genes, gene_ends):
+    num_false_positives, num_true_positives = positive(genes, gene_ends)
+
+    # TPR = TP/TP+FN
+    # TP + FN = CDS
     tp_rate = num_true_positives*1.0/len(gene_ends)
 
-    # FPR += FP/FP+TN 
-    # TN = orfs not meeting threshold or in cds 
+    # FPR += FP/FP+TN
+    # TN = orfs not meeting threshold or in cds
     fp_rate = num_false_positives*1.0/(len(orfs)-len(genes)-len(gene_ends)+num_true_positives)
-    
-    return (fp_rate, tp_rate)
 
-# 3 ######################################################################
+    return (num_false_positives, num_true_positives, fp_rate, tp_rate)
 
 # true-positive: a gene, and says is a gene
 def positive(orfs, gene_ends):
     num_false_positives = 0
     num_true_positives = 0
     for orf in orfs:
-        seq, start, end = orf 
+        seq, start, end = orf
         if end in gene_ends:
             num_true_positives += 1
-        else: 
+        else:
             num_false_positives +=1
     return (num_false_positives, num_true_positives)
+
+# 3 ######################################################################
 
 def len_thresh(orfs, gene_ends, threshold):
     genes = []
     for orf in orfs:
-        seq, start, end = orf 
+        seq, start, end = orf
         if len(seq) > threshold:
             genes.append(orf)
-    
-    num_false_positives, num_true_positives = positive(genes, gene_ends)
 
-    return get_rates(num_true_positives, num_false_positives, orfs, genes, gene_ends)
-    
+    return get_rates(orfs, genes, gene_ends)
+
+def reach_len_thresh(orfs, gene_ends, threshold):
+    num_false_positives, num_true_positives, fp_rate, tp_rate = len_thresh(orfs, gene_ends, threshold)
+    tp_percent = tp_rate * 100
+    print "LENGTH THRESHOLD of " + str(threshold) + " --> " + str(tp_percent) + "% true positive rate, " + str(num_true_positives) + " true positives, " + str(num_false_positives) + " false positives"
+    print ""
+
 # 4 ######################################################################
- 
- def mm_thresh(orfs, gene_ends, threshold, k, trusted_genes, trusted_non_genes):
+
+def mm_thresh(orfs, gene_ends, threshold, k, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs):
+    genes = []
+    for orf in orfs:
+        seq, start, end = orf
+        mm_score = markov_model.calc_mm_score(seq, k, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs)
+        if mm_score > threshold:
+            genes.append(orf)
+
+    return get_rates(orfs, genes, gene_ends)
+
+def reach_mm_thresh(orfs, gene_ends, threshold, k, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs):
+    num_false_positives, num_true_positives, fp_rate, tp_rate = mm_thresh(orfs, gene_ends, threshold, k, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs)
+    tp_percent = tp_rate * 100
+    print "MM SCORE THRESHOLD of " + str(threshold) + " --> " + str(tp_percent) + "% true positive rate, " + str(num_true_positives) + " true positives, " + str(num_false_positives) + " false positives"
+    print ""
+
+# 5 ######################################################################
+
+def plot_scatter(k, orfs, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs, gene_ends, thresh):
+    plt.suptitle('Combining Length and MM Scores')
+    plt.xlabel('ORF Length')
+    plt.ylabel('Markov Model Score')
+
+    trusted_gene_x, trusted_gene_y = plot_scatter_helper(k, trusted_genes, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs)
+
+    trusted_non_gene_x, trusted_non_gene_y = plot_scatter_helper(k, trusted_non_genes,trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs)
+
+    all_gene_x, all_gene_y, all_non_gene_x, all_non_gene_y = plot_scatter_distribute(k, orfs, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs, gene_ends)
+
+    # plt.scatter(trusted_gene_x, trusted_gene_y, color='orange')
+    # plt.scatter(trusted_non_gene_x, trusted_non_gene_y, color='blue')
+    plt.scatter(all_gene_x, all_gene_y, color='orange')
+    plt.scatter(all_non_gene_x, all_non_gene_y, color='blue')
+
+    a_x, a_y, b_x, b_y, m, b = calc_median(trusted_gene_x, trusted_gene_y, trusted_non_gene_x, trusted_non_gene_y)
+
+    p1_x, p1_y, p2_x, p2_y, p_m = calc_perpendicular(a_x, b_x, m, b, 0.24)
+    num_false_positives, num_true_positives, fp_rate, tp_rate = get_positives_with_equation(k, orfs, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs, gene_ends, p1_x, p1_y, p_m)
+    print "false positives:", num_false_positives, "true positives:", num_true_positives, "fpr:", fp_rate, "tpr:", tp_rate
+
+    # median line 
+    plt.annotate('A', (a_x, a_y), color='cyan')
+    plt.annotate('B', (b_x, b_y), color='cyan')
+    plt.plot([a_x, b_x], [a_y, b_y], color='cyan')
+
+    # perpendicular line 
+    plt.plot([p1_x, p2_x], [p1_y, p2_y], color='green')
+
+    # draw lines x = 50, x = 1400
+    plt.plot([50, 50], [-400, 300], color='red')
+    plt.plot([1400, 1400], [-400, 300], color='magenta')
+
+    plt.legend(['y = 0.0342799012441x - 0.63156280769 (median)', 'y - 11.6680657587 = m(x - 358.8) (perpendicular)', 'x = 50 (short ORF threshold)', 'x = 1400 (long ORF threshold)', 'genes', 'non-genes'], loc='lower right')
+    
+    plt.show()
+
+def plot_scatter_helper(k, orfs, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs):
+    x = []
+    y = []
+    for seq in orfs:
+        x_pt = len(seq)
+        y_pt = markov_model.calc_mm_score(seq, K, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs)
+        x.append(x_pt)
+        y.append(y_pt)
+    return (x, y)
+
+def plot_scatter_distribute(k, orfs, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs, gene_ends):
+    gene_x = []
+    gene_y = []
+    non_gene_x = []
+    non_gene_y = []
+
+    for orf in orfs: 
+        seq, start, end = orf 
+        x_pt = len(seq)
+        y_pt = markov_model.calc_mm_score(seq, K, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs)
+        if end in gene_ends:
+            gene_x.append(x_pt)
+            gene_y.append(y_pt)
+        else:
+            non_gene_x.append(x_pt)
+            non_gene_y.append(y_pt)
+    
+    return (gene_x, gene_y, non_gene_x, non_gene_y)
+
+def calc_median(trusted_gene_x, trusted_gene_y, trusted_non_gene_x, trusted_non_gene_y):
+    a_x = np.median(np.array(trusted_non_gene_x))
+    a_y = np.median(np.array(trusted_non_gene_y))
+    b_x = np.median(np.array(trusted_gene_x))
+    b_y = np.median(np.array(trusted_gene_y))
+
+    m = (a_y - b_y) / (a_x - b_x)
+    b = a_y - m*a_x
+
+    print "Median Line"
+    print "A: (" + str(a_x) + ", " + str(a_y) + ")"
+    print "B: (" + str(b_x) + ", " + str(b_y) + ")"
+    print "slope: " + str(m)
+    print "equation: y = " + str(m) + "x + " + str(b)
+    print "" 
+
+    return (a_x, a_y, b_x, b_y, m, b)
+
+def calc_perpendicular(a_x, b_x, m, b, percent): 
+    # intercept = % into slope
+    pi_x = a_x + percent * (b_x - a_x)
+    pi_y = m * pi_x + b
+    
+    # slope = negative recipricol
+    p_m = -1 * np.reciprocal(m)
+
+    # y - y1 = m(x - x1) --> y = mx -mx1 + y1
+    # get 2 points on the line 
+    p1_x = pi_x + 13
+    p1_y = calc_y_coord(p1_x, pi_x, pi_y, p_m)
+    p2_x = pi_x - 10
+    p2_y = calc_y_coord(p2_x, pi_x, pi_y, p_m)
+
+    print "Perpendicular Line"
+    print "intercept: (" + str(pi_x) + ", " + str(pi_y) + ")"
+    print "slope:", p_m
+    print "equation: y -", pi_y, "= m(x -", pi_x, ")"
+    print "point 1: (", p1_x, ",", p1_y, ")"
+    print "point 2: (", p2_x, ",", p2_y, ")"
+    print "" 
+
+    return (p1_x, p1_y, p2_x, p2_y, p_m)
+
+def calc_y_coord(x, x1, y1, m):
+    return (m * x) - (m * x1) + y1
+
+def get_positives_with_equation(k, orfs, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs, gene_ends, x1, y1, m):
     genes = []
     for orf in orfs: 
         seq, start, end = orf 
-        mm_score = markov_model.calc_mm_score(seq, k, trusted_genes, trusted_non_genes)
-        if mm_score > threshold: 
+        x = len(seq)
+        y = markov_model.calc_mm_score(seq, K, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs)
+        y_thresh = calc_y_coord(x, x1, y1, m)
+        if y > y_thresh:
             genes.append(orf)
 
-    num_false_positives, num_true_positives = positive(genes, gene_ends)
-
-    return get_rates(num_true_positives, num_false_positives, orfs, genes, gene_ends)
-
-def mm_thresh_2(orfs, gene_ends, threshold, k, trusted_genes, trusted_non_genes, mm_scores):
-    genes = []
-    for mm_score in mm_scores: 
-        orf, score = mm_score 
-        if score > threshold: 
-            genes.append(orf) 
-    num_false_positives, num_true_positives = positive(genes, gene_ends)
-    return get_rates(num_true_positives, num_false_positives, orfs, genes, gene_ends) 
+    return get_rates(orfs, genes, gene_ends)
 
 # ERROR CHECKING #########################################################
 
 def check_genes(orfs, gene_ends):
     count = 0
-    for orf in orfs: 
+    for orf in orfs:
         subseq, start, end = orf
         if end in gene_ends:
             count += 1
-    print "matching ends:", count 
+    print "matching ends:", count
     print ""
 
 # EXECUTE PROGRAM ########################################################
 
+start_time = time.time()
 main()
 print("--- %s seconds ---" % (time.time() - start_time))
-
-# x1 = [39, 6, 15, 12, 15]
-# x2 = [1614, 1623, 2388, 1422, 1665]
-# y1 = [1.56678727191, 0.667366861992, 0.633938280251, 0.418546201372, 0.634136080416]
-# y2 = [54.5409415958, 65.6859640015, 76.0581583166, 36.7210402482, 61.6096434786]
-# plt.scatter(x1, y1, color='blue')
-# plt.scatter(x2, y2, color='orange')
-
-# x1_med = x1[2]
-# y1_med = y1[2]
-# x2_med = x2[2]
-# y2_med = y2[2]
-# print "(" + str(x1_med) + ", " + str(y1_med) + ")"
-# print "(" + str(x2_med) + ", " + str(y2_med) + ")"
-
-# plt.plot([x1_med, x2_med], [y1_med, y2_med])
-
-# plt.show()
-
-# x = [0.56, 0.78, 0.91]
-# y = [0.01, 0.19, 0.58]
-# plt.plot(y, x, 'ro')
-# plt.show()
-
--- markov_model.py
+++ markov_model.py
@@ -4,11 +4,10 @@ import re
 
 def calc_mm_score(x, k, trusted_genes, trusted_non_genes, trusted_genes_probs, trusted_non_genes_probs):
     log_p_x = calc_prob(x, k, trusted_genes, trusted_genes_probs)
-    # print "log_p_x", log_p_x
     log_q_x = calc_prob(x, k, trusted_non_genes, trusted_non_genes_probs)
-    # print "log_q_x", log_q_x 
     return log_p_x - log_q_x
 
+# k = 3 --> P(x) = P(x1 x2 x3) * P(x4 | x1 x2 x3) * P(x5 | x2 x3 x4) ... P(xn | xn-3 xn-2 xn-1)
 def calc_prob(x, k, training, probs):
     prob = probs[x[:k]]
     log_prob = math.log(prob, 2)
@@ -17,57 +16,18 @@ def calc_prob(x, k, training, probs):
         log_prob += math.log(sub_prob, 2)
     return log_prob
 
-# k = 3 --> P(x) = P(x1 x2 x3) * P(x4 | x1 x2 x3) * P(x5 | x2 x3 x4) ... P(xn | xn-3 xn-2 xn-1)
-# def calc_prob(x, k, training):
-#     prob = uncondit_prob(x[:k], k, training)
-#     # print "u_prob", prob
-#     log_prob = math.log(prob, 2)
-
-#     for idx in xrange(0, len(x)-k):
-#         sub_prob = condit_prob(x[idx+k], x[idx:idx+k], k, training)
-#         # print "s_prob", sub_prob
-#         log_prob += math.log(sub_prob, 2)
-
-#     return log_prob
-
 # k = 3 --> P(x) = P(AAA), e.g.
 # assume x is a sequence of k nucleotides
 def uncondit_prob(x, k, training):
     num_x = 0
     total = 0
     for seq in training:
-        # num_x += len(re.findall(r'(a)(?=\1)', x))
-        # num_x += sum((seq[i:i+k])==x for i in xrange(len(seq)))
-        # total += len(seq)-k+2
-        # num_x += seq.coun()
         for i in xrange(len(seq)-(k-1)):
             sub_seq = seq[i:i+k]
             if sub_seq == x:
                 num_x += 1
             total += 1
     return num_x/(total * 1.0)
-
-    # num_x = sum((seq[i:i+k])==x for i in xrange(len(seq))))
-
-# def condit_prob(x, y, k, training):
-#     y_seqs = find_y(y, k, training)
-#     num_y = len(y_seqs)
-#     num_x = count_x(x, k, y_seqs)
-#     return num_x/(num_y * 1.0)
-
-# # k = 3 --> P(x|y) = P(A|AA), e.g.
-# def condit_prob(x, y, k, training):
-#     num_x = 0
-#     num_y = 0
-#     for seq in training:
-#         for i in xrange(len(seq)-k):
-#             sub_seq = seq[i:i+k+1]
-#             # if the first 3 letters of the subsequence is y, check if the last letter is x
-#             if sub_seq[:k]==y:
-#                 num_y += 1
-#                 if sub_seq[-1]==x:
-#                     num_x += 1
-#     return num_x/(num_y * 1.0)
 
 def condit_prob(x, y, k, training):
     # P(x|y) = P(xy)/P(y)
@@ -88,10 +48,6 @@ def find_y_helper(y, k, seq, y_seqs):
         sub_seq = seq[i:i+k+1]
         if sub_seq[:k]==y:
             y_seqs.append(sub_seq)
-
-# find all seqs ___x
-def count_x(x, k, seqs):
-    return sum(1 if seq[-1]==x else 0 for seq in seqs)
 
 # count seqs y1y2y3x1
 def count_yx(yx, k, seqs): 
